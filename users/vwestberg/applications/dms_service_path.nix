e4e9e9f67bf4848c8b0b5ccf9418c06cd5847090
vwestberg <vwestberg@lselectric.com>
2026-05-21 09:17:40 -0500

    Fix dms.service Environment so qs is discoverable via PATH drop-in

--- users/vwestberg/applications/dms_service_path.nix
+++ users/vwestberg/applications/dms_service_path.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  config.systemd.user.services.dms.path = [ pkgs.quickshell ];
+  config.systemd.user.services.dms.serviceConfig.Environment = [
+    "PATH=${lib.makeBinPath [ pkgs.quickshell ]}:/run/current-system/sw/bin"
+  ];
 }
