da2d99c74672f2653f2689924433e624e1c94056
vwestberg <vwestberg@lselectric.com>
2026-07-16 13:03:01 -0500

    ninjaone: update ncplayer client to 14.35.8480

--- users/vwestberg/applications/ninjaone_config.nix
+++ users/vwestberg/applications/ninjaone_config.nix
@@ -2,7 +2,7 @@
 {
   programs.ninjaone = {
     enable = true;
-    deb_path = /nix/store/hpwnxc53dn2f94qgcgmasrmv7649n6qf-ninjarmm-ncplayer_amd64.deb;
+    deb_path = /nix/store/giby5b82bkrs5z6n4zpj3pb2x4hhv1qq-ninjarmm-ncplayer_amd64.deb;
     update_alias.enable = true;
     reset_browser_alias.enable = true;
   };
